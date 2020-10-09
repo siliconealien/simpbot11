cc5afc84353e76ed624ead79a1ae43644494b489
Liza Shankar <LizaKaliShankar@gmail.com>
2020-10-09 01:38:17 -0400

    Credit added to help

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ client.on('message', message => {
   } else if (message.content.startsWith(config.prefix + "nyaa")) {
 	  message.channel.send('Hewwo Jon >:3c');
 	} else if (message.content.startsWith(config.prefix + "s4shelp")) {
-		message.channel.send('Use `!s4splay` to start a game of Simp 4 Simp!\n\n**My dear Jonah ...**\n(Or, "What does this button do??")\n`!beholdme`\n`!nyaa`\n`!pipeme`');
+		message.channel.send('Use `!s4splay` to start a game of Simp 4 Simp!\n\n**My dear Jonah ...**\n(Or, "What does this button do??")\n`!beholdme`\n`!nyaa`\n`!pipeme`\n\n`!s4sicon` - Icon artist credit');
 	} else if (message.content.includes('Jurgen Leitner')) {
 	  message.react('714387828913733643');
 }
