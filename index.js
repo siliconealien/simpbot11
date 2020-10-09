54544dd0bd26de640077dc85b9503ffa5d0b7010
Liza Shankar <LizaKaliShankar@gmail.com>
2020-10-08 20:41:45 -0400

    Cheap whistle

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const config = require("./config.json");
 
 client.once('ready', () => {
 	console.log('Ready!');
+  client.user.setActivity('you like a cheap whistle');
 });
 
 client.on('message', message => {
