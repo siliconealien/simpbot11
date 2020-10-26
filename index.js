81ceb31c96e2cba2ee6434802dc6bd1341ed6b58
Leslie Behum <llaurenb@gmail.com>
2020-10-26 18:03:01 -0400

    test

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const { checkServerIdentity } = require('tls');
 // client.commands = new Discord.Collection();
 // const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-// for (const file of commandFiles) {
-// 	const command = require(`./commands/${file}`);
-// 	client.commands.set(command.name, command)
-// }
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command)
+}
 
 client.once('ready', () => {
 	console.log('Ready!');
