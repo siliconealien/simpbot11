733a619c3968c8ed528014641024a6bccf4a7195
Kaly <71524855+kaly666@users.noreply.github.com>
2021-02-22 01:54:24 +0200

    Delete jmseduce.js

--- commands/jmseduce.js
+++
@@ -1,8 +0,0 @@
-// module.exports = {
-//     name: 'jmSeduce',
-//     description: 'jmSeduce',
-//     execute(message, args) {
-//         message.channel.send('`You nod toward an empty hallway and allow Jonah to lead you there, his hand still lingering at your waist.  "I think you must know what I am after," you say in a sultry tone, and Jonah\'s eyes widen, his smile broadening.  "Your ... hands-on approach as a host has not gone unnoticed by me tonight."`\n\n`"You waste no time, do you?" Jonah breathes, but he is visibly thrilled by your audacity.  Without warning, he collapses against you into the wall, kissing you deeply and with teeth.  You cannot tear off each other\'s clothes fast enough.  As he hikes your thighs up to wrap around him and penetrates you, you imagine you can feel an infinite number of eyes catching you in the act, and it is this that makes you come the first time.`\n\n`This illicit relationship continues for months, but you two never marry, and he never says he loves you.  But he is deeply affectionate, and he takes great joy in making you scream his name.  You would adore the time spent with him, but he has a horrible habit of bringing his work home with him, and you do so hate hearing the dreadful stories that he is always so eager to whisper in your ear late at night.`\n\n`Ultimately, you die young.  The autopsy yields unpleasant results, to say the least.`\n\n`==========THE END==========`\n`* You became Jonah Magnus\'s lover`\n`* You later died for creepy Eye-related reasons`\n`Not Bad.`
-// )
-//     },
-// };
