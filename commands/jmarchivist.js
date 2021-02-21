d841951491b10be6d55f533adb18cc4c8fe30c2a
Kaly <71524855+kaly666@users.noreply.github.com>
2021-02-22 01:54:06 +0200

    Delete jmarchivist.js

--- commands/jmarchivist.js
+++
@@ -1,8 +0,0 @@
-// module.exports = {
-//     name: 'jmArchivist',
-//     description: 'jmArchivist',
-//     execute(message, args) {
-//         message.channel.send('`"Yes," you gasp, feeling in the darkest reaches of your true self a remarkable sense of purpose.  "I want you to take me with you."`\n\n`With your assistance (and all the subsequent trauma that entails for you), Jonah discovers the secret of the ritual much sooner than he would have otherwise.  You become the Archivist.  Together, you open the door long before he ever might have needed to body-hop.`\n\n`An hour passes, or a day, or a millennium ... there is no way to say in the ruined world.  But you spend that time on your lover\'s lap, sharing the eldritch throne, two guiltless monsters ruling over all.`\n\n`And you could not ask for more.`\n\n`==========THE END==========`\n`Congratulations!`\n`* You became the first and last Archivist`\n`* You helped Jonah Magnus bring about The Change`\n`* You and Jonah are unrepentant abominable lovers who should feel bad`\n`Great Job!`
-// )
-//     },
-// };
