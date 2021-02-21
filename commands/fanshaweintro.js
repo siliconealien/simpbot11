b7d49f18d4c4af482932e46d38bff7aab5cb14cf
Kaly <71524855+kaly666@users.noreply.github.com>
2021-02-22 01:53:16 +0200

    Delete fanshaweintro.js

--- commands/fanshaweintro.js
+++
@@ -1,8 +0,0 @@
-// module.exports = {
-//     name: 'fanshaweIntro',
-//     description: 'fanshaweIntro',
-//     execute(message, args) {
-//         message.channel.send('`"Ahh," says Barnabas appreciatively, "that\'s Dr. Fanshawe, Jonah\'s personal physician."`\n\n`"Handsome devil," you say with a smirk.`\n\n`"Truly one worth sinning for," Barnabas says with a mischievous wink.  (You realize affectionately that Barnabas has just become your new queer best friend.)`\n\n`"Have you met him?" you ask.`\n\n`"Oh, no," Barnabas says, scandalized, "I don\'t exactly share his pedigree."`\n\n`You feel a protective flare of anger on Barnabas\'s behalf, and you spend the next few minutes convincing him that he is just as good as Fanshawe and the two of you are going to march over there right now, dammit.  In the end, you practically drag him, but you get there.`\n\n`Dr. Fanshawe turns out to be perfectly polite, in spite of his resting bitch face, and he insists you call him Jonathan.  "Thank you for the introduction," he says kindly, smiling at Barnabas out of the corner of his eye.  "You cannot know how you have just saved me from myself."`
-// )
-//     },
-// };
