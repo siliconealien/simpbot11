2395a71468484c91df7c72ee7f0b4e3aaf01dfa6
Liza Shankar <>
2021-02-22 04:02:25 -0500

    removed repetitive lines from Fanshawe route

--- index.test.js
+++
@@ -1,8 +0,0 @@
-
-var myMod = require('./index');
-var gameState = myMod.gameState;
-
-test('the shopping list has beer on it', () => {
-    expect(gameState).toContain('sbhelp');
-    expect(new Set(gameState)).toContain('sbhelp');
-});
