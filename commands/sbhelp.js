e85090aa86fccbf8aaaa0881cdd374f8d3b29719
Liza Shankar <>
2021-03-01 22:42:00 -0500

    update help

--- commands/sbhelp.js
+++ commands/sbhelp.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'sbhelp',
     description: 'sbhelp',
     execute(message, args) {
-        message.channel.send('Use `!s4splay` to start a game of Simp 4 Simp!\n\n**My dear Jonah ...**\n(Or, "What does this button do??")\n`!bastard`\n`!beholdme`\n`!nyaa`\n`!pipeme`\n\n`!sbfaq` - more info\n`!sbicon` - icon artist credit\n\n:eye: *Please follow @simpbot11 on Twitter!* :eye:')
+        message.channel.send('Use `!s4splay` to start a game of Simp 4 Simp!\n\n**My dear Jonah ...**\n(Or, "What does this button do??")\n`!bastard`\n`!beholdme`\n`!fmk`\n`!nyaa`\n`!pipeme`\n\n`!sbfaq` - more info\n`!sbicon` - icon artist credit\n\n:eye: *Please follow @simpbot11 on Twitter!* :eye:')
     },
 };
