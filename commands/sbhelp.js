c8c873d737b97b4fa31d0c7b85a023ba64b6f6ed
Liza Shankar <>
2021-03-09 04:12:46 -0500

    added big to commands list

--- commands/sbhelp.js
+++ commands/sbhelp.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'sbhelp',
     description: 'sbhelp',
     execute(message, args) {
-        message.channel.send('Use `!s4splay` to start a game of Simp 4 Simp!\n\n**My dear Jonah ...**\n(Or, "What does this button do??")\n`!bastard`\n`!beholdme`\n`!fmk`\n`!nyaa <@user>`\n`!pipeme`\n\n*The **new*** `!nyaa` *command is more cursed than ever!  **You\'re welcome!!***\n\n`!sbfaq` - more info\n`!sbart` - Simp-bot artist credit\n`!sbgive`\n\n:eye: *Please follow @simpbot11 on Twitter!* :eye:')
+        message.channel.send('Use `!s4splay` to start a game of Simp 4 Simp!\n\n**My dear Jonah ...**\n(Or, "What does this button do??")\n`!bastard`\n`!beholdme`\n`!big`\n`!fmk`\n`!nyaa <@user>`\n`!pipeme`\n\n*The **new*** `!nyaa` *command is more cursed than ever!  **You\'re welcome!!***\n\n`!sbfaq` - more info\n`!sbart` - Simp-bot artist credit\n`!sbgive`\n\n:eye: *Please follow @simpbot11 on Twitter!* :eye:')
     },
 };
