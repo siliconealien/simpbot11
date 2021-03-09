740ce9c8e18fd0a8eef4ce4d1b638d24595c0134
Liza Shankar <>
2021-03-08 21:52:33 -0500

    an attempt to simplify might need to undo

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 require('dotenv').config();
 const token = process.env.TOKEN;
 const fs = require('fs');
-const Discord = require('discord.js');
-const { Client, MessageEmbed } = require('discord.js');
+const { Discord, Client, MessageEmbed } = require('discord.js');
 const { prefix } = require("./config.json");
 const { checkServerIdentity } = require('tls');
 const story = require("./story/simpforsimp.json")
